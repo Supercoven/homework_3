ad53f3145a3cf551f6d6e7acf2c93baa331e4cc8
Burundukov Denis <burundukov.d@yandex.ru>
2022-09-02 12:11:40 +0500

    19 completed

--- Task 19/Program.cs
+++ Task 19/Program.cs
@@ -4,3 +4,19 @@
 //12821 -> да
 //23432 -> да
 
+Console.WriteLine("Введите пятизначное число");
+int num = int.Parse(Console.ReadLine());
+string forCheck = num.ToString();
+
+if (forCheck.Length == 5);
+{
+    int firstDigit = num /10000; // первая цифра с начала
+    int firstSecondDigit = num /1000; // первая и вторая цифра с начала
+    int secondDigit = firstSecondDigit % 10; // вторая цифра с начала
+
+    int lastDigit = num % 10; // последняя цифра с конца
+    int twoLastDigits = num % 100;
+    int preLastDigit = twoLastDigits / 10; // вторая цифра с конца
+
+}
+    
